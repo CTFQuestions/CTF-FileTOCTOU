52d02b3b3a504d4ddb759af61a1137654fc1c3e0
Yang <113082249+1234567Yang@users.noreply.github.com>
2026-08-19 00:15:12 -0700

    Update create_user_and_service.sh

--- create_user_and_service.sh
+++ create_user_and_service.sh
@@ -2,9 +2,9 @@
 # Alpine create user
 
 # -D No interactive password
-adduser -D guest
+adduser -D challenger
 
 # chpasswd 从标准输入读 "用户名:密码"，绕开 passwd 的交互提示
-echo "guest:123456" | chpasswd
+echo "challenger:qwerty114514" | chpasswd
 
-echo "user [guest] created，password 123456"
+echo "user [guest] created，password qwerty114514"
